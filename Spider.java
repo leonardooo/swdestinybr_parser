393ac38d90a958d9ed40d722ddbae9d77449104b
Leonardo de Assis <leonardooo@gmail.com>
2018-08-14 11:04:20 -0300

    lista de banidas completa e Spider atualizado

--- Spider.java
+++ Spider.java
@@ -1,7 +1,23 @@
 import java.io.*;
+import java.util.*;
 class Spider {
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        BufferedReader br = new BufferedReader(new FileReader("banidas.txt"));
+        Set<String> banidas = new HashSet<>();
+
+        while(true) {
+            String linha = br.readLine();
+
+            if(linha == null || linha.trim().equals("")) {
+                break;
+            }
+
+            banidas.add(linha.trim().toLowerCase());
+        }
+
+        br.close();
+        br = new BufferedReader(new InputStreamReader(System.in));
 
         while(true) {
             String linha = br.readLine();
@@ -16,7 +32,7 @@ class Spider {
 
             Double valor = Double.parseDouble(linha3_array[2]);
 
-            if(valor <= 5) {
+            if(valor <= 5 && !banidas.contains(nome.trim().toLowerCase())) {
                 System.out.println(nome + " " + valor);
             }
         }
